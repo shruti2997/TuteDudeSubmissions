e51494c0ddf695b6a51bf7e350438d30c88baff5
Shruti Das <shrutidas291297@gmail.com>
2026-01-11 22:52:16 +0530

    Add files via upload
    ASSIGNMENT 1
    MODULE 2 : BASIC PYTHON CONCEPTS
    TASK 1 : PERFORM BASIC MATHEMATICAL OPERATIONS

---
+++ ASSIGNMENT1TASK1.py
@@ -0,0 +1,11 @@
+# PERFORM BASIC MATHEMATICAL OPERATIONS
+
+A = input("Enter a number:  ")
+B = input("Enter a number:  ")
+
+Addition = int(A) + int(B)
+Subtraction = int(A) - int(B)
+Multiplication = int(A) * int(B)
+Division = float(A) / float(B)
+
+print("Addition:",Addition, "\nSubtraction:", Subtraction, "\nMultiplication:", Multiplication, "\nDivision:",Division)
