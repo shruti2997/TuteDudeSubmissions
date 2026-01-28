6a43489d2dad8d535c64a97b424fb394cb5954e2
Shruti Das <shrutidas291297@gmail.com>
2026-01-28 12:59:18 +0530

    Add files via upload

---
+++ ASSIGNMENT 2 TASK 2 (SUM LOOP).py
@@ -0,0 +1,5 @@
+#  SUM OF INTEGERS FROM 1 TO 50 USING A FOR LOOP
+Total_of_integers = 0
+for n in range(1, 50):
+    Total_of_integers += n
+print(f'The sum of numbers from 1 to 50 is {Total_of_integers} ')
