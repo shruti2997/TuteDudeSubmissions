e81293bf23e803eeed5705783ea6caa29626366e
Shruti Das <shrutidas291297@gmail.com>
2026-02-05 20:32:04 +0530

    Add files via upload

---
+++ ASSIGNMENT3 TASK2 MATH MODULE FOR CALCULATION.py
@@ -0,0 +1,13 @@
+# MATH MODULE FOR CALCULATION
+# CALCULATE - SQUARE ROOT, NATURAL LOGARITHM(log base e), SINE (in radians)
+
+import math
+Given_number = float(input("Enter a number: "))
+square_root = math.sqrt(Given_number)
+print(f"Square root: {square_root}")
+
+natural_logarithm = math.log(Given_number)  #natural log base e
+print(f"Logarithm: {natural_logarithm}")
+
+sine_rad = math.sin(Given_number)
+print(f"Sine: {sine_rad}")
